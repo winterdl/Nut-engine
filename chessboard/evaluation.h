1b67df84ef2e0d0a493713ccb19735388e492632
Chijun Sima <1300339379@qq.com>
2018-01-11 13:17:13 +0800

    fix abi

--- chessboard/evaluation.h
+++ chessboard/evaluation.h
@@ -1,29 +1,32 @@
 /*
-   Copyright 2017 Chijun Sima
+Copyright 2017 Chijun Sima
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-	   http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 #pragma once
 #include "stdafx.h"
+#include "chessboard.h"
 
-#define DllExport __declspec(dllexport)
+#ifndef DllExport
+#define DllExport   __declspec( dllexport )  
+#endif // !DllExport
 
-class DllExport evaluation
+class evaluation
 {
 public:
-	evaluation() noexcept {};
-	int evaluate(chessboard &, const int turn, const int row, const int col, bool pure) noexcept;
+	DllExport evaluation() noexcept {}
+	DllExport int evaluate(chessboard &, const int turn, const int row, const int col, bool pure) noexcept;
 
 private:
 	void reset_point(chessboard &board, int row, int col, bool pure) noexcept;
@@ -46,11 +49,12 @@ private:
 	}
 	int __evaluate(chessboard &, int turn, const int row, const int col, bool pure) noexcept;
 	void evaluate_point(chessboard &board, int row, int col) noexcept;
-	const uint8_t STWO = 1, STHREE = 2, SFOUR = 3, TWO = 4, THREE = 5, FOUR = 6, FIVE = 7, DFOUR = 8,
-		FOURT = 9, DTHREE = 10, NOTYPE = 11, ANALYSED = 255, TODO = 0;
+	const uint8_t STWO = 1, STHREE = 2, SFOUR = 3, TWO = 4, THREE = 5, FOUR = 6, FIVE = 7,
+		ANALYSED = 255, TODO = 0;
 	const uint8_t BLACK = 1, WHITE = 2;
 	const std::vector<uint8_t> nturn = { 0, 2, 1 };
 	std::vector<uint8_t> result = std::vector<uint8_t>(15);
 	uint8_t flag;
 	std::vector<uint8_t>copy_place = std::vector<uint8_t>(30);
 };
+
